657381537388a3c92fa0e029acd7b7944f5396db
GiackAloZ <giacomo.aloisi1998@gmail.com>
2019-04-27 15:25:10 +0200

    add unit of measure

---
+++ nwoolcan-giackaloz/src/UnitTests/Utils/TestUnitOfMeasure.cs
@@ -0,0 +1,55 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NWoolcan.Utils;
+
+namespace NWoolcan.UnitTests.Utils
+{
+    [TestClass]
+    public class TestUnitOfMeasure
+    {
+
+        [TestMethod]
+        public void Symbols()
+        {
+            Assert.AreEqual("L", UnitOfMeasure.Liter.GetSymbol());
+        }
+
+        [TestMethod]
+        public void Validations()
+        {
+            Assert.IsTrue(UnitOfMeasure.Liter.Validate(10));
+            Assert.IsTrue(UnitOfMeasure.Liter.Validate(10.1));
+            Assert.IsTrue(UnitOfMeasure.Liter.Validate(0));
+            Assert.IsFalse(UnitOfMeasure.Liter.Validate(-10));
+            
+            Assert.IsTrue(UnitOfMeasure.Gram.Validate(10));
+            Assert.IsTrue(UnitOfMeasure.Gram.Validate(10.1));
+            Assert.IsTrue(UnitOfMeasure.Gram.Validate(0));
+            Assert.IsFalse(UnitOfMeasure.Gram.Validate(-10));
+            
+            Assert.IsTrue(UnitOfMeasure.Bottle33Cl.Validate(10));
+            Assert.IsFalse(UnitOfMeasure.Bottle33Cl.Validate(10.1));
+            Assert.IsTrue(UnitOfMeasure.Bottle33Cl.Validate(0));
+            Assert.IsFalse(UnitOfMeasure.Bottle33Cl.Validate(-10));
+            
+            Assert.IsTrue(UnitOfMeasure.Bottle50Cl.Validate(10));
+            Assert.IsFalse(UnitOfMeasure.Bottle50Cl.Validate(10.1));
+            Assert.IsTrue(UnitOfMeasure.Bottle50Cl.Validate(0));
+            Assert.IsFalse(UnitOfMeasure.Bottle50Cl.Validate(-10));
+            
+            Assert.IsTrue(UnitOfMeasure.Bottle66Cl.Validate(10));
+            Assert.IsFalse(UnitOfMeasure.Bottle66Cl.Validate(10.1));
+            Assert.IsTrue(UnitOfMeasure.Bottle66Cl.Validate(0));
+            Assert.IsFalse(UnitOfMeasure.Bottle66Cl.Validate(-10));
+            
+            Assert.IsTrue(UnitOfMeasure.Bottle75Cl.Validate(10));
+            Assert.IsFalse(UnitOfMeasure.Bottle75Cl.Validate(10.1));
+            Assert.IsTrue(UnitOfMeasure.Bottle75Cl.Validate(0));
+            Assert.IsFalse(UnitOfMeasure.Bottle75Cl.Validate(-10));
+            
+            Assert.IsTrue(UnitOfMeasure.BottleMagnum.Validate(10));
+            Assert.IsFalse(UnitOfMeasure.BottleMagnum.Validate(10.1));
+            Assert.IsTrue(UnitOfMeasure.BottleMagnum.Validate(0));
+            Assert.IsFalse(UnitOfMeasure.BottleMagnum.Validate(-10));
+        }
+    }
+}
---
+++ nwoolcan-giackaloz/src/Utils/UnitOfMeasure.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+namespace NWoolcan.Utils
+{
+    public enum UnitOfMeasure
+    {
+        Liter, Gram, Bottle33Cl, Bottle50Cl, Bottle66Cl, Bottle75Cl, BottleMagnum
+    }
+
+    static class UnitOfMeasureExtensions
+    {
+        public static bool IsPositive(this double v) => v >= 0;
+
+        public static bool IsInteger(this double v) => v.Equals(Math.Floor(v));
+        
+        private static Dictionary<UnitOfMeasure, string> _symbols = new Dictionary<UnitOfMeasure, string>(
+            new List<KeyValuePair<UnitOfMeasure, string>>
+            {
+                new KeyValuePair<UnitOfMeasure, string>(UnitOfMeasure.Liter, "L"),
+                new KeyValuePair<UnitOfMeasure, string>(UnitOfMeasure.Gram, "g"),
+                new KeyValuePair<UnitOfMeasure, string>(UnitOfMeasure.Bottle33Cl, "bot 33cl"),
+                new KeyValuePair<UnitOfMeasure, string>(UnitOfMeasure.Bottle50Cl, "bot 50cl"),
+                new KeyValuePair<UnitOfMeasure, string>(UnitOfMeasure.Bottle66Cl, "bot 66cl"),
+                new KeyValuePair<UnitOfMeasure, string>(UnitOfMeasure.Bottle75Cl, "bot 75cl"),
+                new KeyValuePair<UnitOfMeasure, string>(UnitOfMeasure.BottleMagnum, "bot magnum"),
+            });
+        
+        private static Dictionary<UnitOfMeasure, Predicate<double>> _validators = new Dictionary<UnitOfMeasure, Predicate<double>>(
+            new List<KeyValuePair<UnitOfMeasure, Predicate<double>>>
+            {
+                new KeyValuePair<UnitOfMeasure, Predicate<double>>(UnitOfMeasure.Liter, n => n.IsPositive()),
+                new KeyValuePair<UnitOfMeasure, Predicate<double>>(UnitOfMeasure.Gram, n => n.IsPositive()),
+                new KeyValuePair<UnitOfMeasure, Predicate<double>>(UnitOfMeasure.Bottle33Cl, n => n.IsPositive() && n.IsInteger()),
+                new KeyValuePair<UnitOfMeasure, Predicate<double>>(UnitOfMeasure.Bottle50Cl, n => n.IsPositive() && n.IsInteger()),
+                new KeyValuePair<UnitOfMeasure, Predicate<double>>(UnitOfMeasure.Bottle66Cl, n => n.IsPositive() && n.IsInteger()),
+                new KeyValuePair<UnitOfMeasure, Predicate<double>>(UnitOfMeasure.Bottle75Cl, n => n.IsPositive() && n.IsInteger()),
+                new KeyValuePair<UnitOfMeasure, Predicate<double>>(UnitOfMeasure.BottleMagnum, n => n.IsPositive() && n.IsInteger()),
+            });
+
+        public static string GetSymbol(this UnitOfMeasure uom) => _symbols[uom];
+
+        public static bool Validate(this UnitOfMeasure uom, double value) => _validators[uom](value);
+    }
+}
